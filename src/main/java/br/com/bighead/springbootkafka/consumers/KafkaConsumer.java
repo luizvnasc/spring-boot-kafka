f13a994f7a10f06ec38e65a8b65e0af880f6e151
luizvnasc <luiz.v.nasc@gmail.com>
2023-06-27 14:10:05 +0100

    added test with conditional inject of a bean

--- src/main/java/br/com/bighead/springbootkafka/consumers/KafkaConsumer.java
+++ src/main/java/br/com/bighead/springbootkafka/consumers/KafkaConsumer.java
@@ -1,16 +1,18 @@
 package br.com.bighead.springbootkafka.consumers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
 public class KafkaConsumer {
 
 
-    @KafkaListener(topics = "test", groupId = "foo",id = "bar")
-    public void readMessage(String message){
-        log.atInfo().log("received message: {}",message);
+    @KafkaListener(topics = "test", groupId = "foo", id = "bar")
+    public void readMessage(String message) {
+        log.atInfo().log("received message: {}", message);
     }
 }
